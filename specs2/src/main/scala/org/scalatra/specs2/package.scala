cb1e5972351d10e1c5867d569d34393d5850866f
Ross A. Baker <baker@alumni.indiana.edu>
2012-05-03 01:34:05 -0400

    Remove some deprecatia inherited from Scalatra.

--- specs2/src/main/scala/org/scalatra/specs2/package.scala
+++
@@ -1,6 +0,0 @@
-package com.github.http4s
-
-package object specs2 {
-  @deprecated("Use com.github.http4s.test.specs2 instead", "2.0.0")
-  type Http4sSpec = test.specs2.Http4sSpec
-}
--- test/src/main/scala/com/github/http4s/test/JettyContainer.scala
+++ test/src/main/scala/com/github/http4s/test/JettyContainer.scala
@@ -15,19 +15,6 @@ trait JettyContainer extends Container {
 
   def servletContextHandler: ServletContextHandler
 
-  @deprecated("use addServlet(Class, String) or addFilter(Class, String)", "2.0.0")
-  def route(klass: Class[_], path: String) = klass match {
-    case servlet if classOf[HttpServlet].isAssignableFrom(servlet) =>
-      addServlet(servlet.asInstanceOf[Class[_ <: HttpServlet]], path)
-    case filter if classOf[Filter].isAssignableFrom(filter) =>
-      addFilter(filter.asInstanceOf[Class[_ <: Filter]], path)
-    case _ =>
-      throw new IllegalArgumentException(klass + " is not assignable to either HttpServlet or Filter")
-  }
-
-  @deprecated("renamed to addServlet", "2.0.0")
-  def route(servlet: HttpServlet, path: String) = addServlet(servlet, path)
-
   def addServlet(servlet: HttpServlet, path: String) =
     servletContextHandler.addServlet(new ServletHolder(servlet), path)
 
@@ -48,10 +35,6 @@ trait JettyContainer extends Container {
 
   def addFilter(filter: Class[_ <: Filter], path: String, dispatches: EnumSet[DispatcherType]): FilterHolder =
     servletContextHandler.addFilter(filter, path, dispatches)
-
-  @deprecated("renamed to addFilter", "2.0.0")
-  def routeFilter(filter: Class[_ <: Filter], path: String) =
-    addFilter(filter, path)
 
   // Add a default servlet.  If there is no underlying servlet, then
   // filters just return 404.
